a2bb2611f5df133b98f71a1ee4f15f6aa1d27a66
Jacob Spade <spade.jacob@Yahoo.com>
2021-11-25 01:07:33 -0800

    setting up data schema

--- routes/api/data.js
+++ routes/api/data.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+// data model
+const Data = require('../../models/Data')
+
 // route Get api/data
 // route description: get all data
-router.get('/', (req, res, next) => {
-  let dataArray = []
-  let cursor = db.collection('data').find()
-  cursor.forEach((doc, error) => {
-    assert.(null, err)
-  })
+router.get('/', (req, res) => {
+  Data.find()
+    .then(data => res.json(data))
 })
 
 
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const data = require('./routes/api/data')
 
 const app = express();
 
