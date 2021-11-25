cafe88b2be5e27e32a9f81c467e089e9ccfc4149
Jacob Spade <spade.jacob@Yahoo.com>
2021-11-24 23:27:42 -0800

    set server up

--- server.js
+++ server.js
@@ -6,3 +6,18 @@ const app = express();
 
 // bodyparser middleware
 app.use(bodyParser.json());
+
+// DB config
+const db = require('./config/keys').mongoURI
+
+// connect to mongo
+mongoose
+  .connect(db)
+  .then(() => console.log('mongoDB connected...'))
+  .catch(err => console.log(err));
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+  console.log(`server started on port ${port}`)
+})
