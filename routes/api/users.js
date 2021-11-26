d02297e14515afc6649a9bf0a5dfbaba7e85895e
Jacob Spade <spade.jacob@Yahoo.com>
2021-11-25 18:13:12 -0800

    added login route

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,12 +6,6 @@ const jwtGenerator = require('../../utils/jwtGenerator')
 // user model
 const User = require('../../models/User')
 
-// route Get api/users
-// route description: get all users
-router.get('/', (req, res) => {
-})
-
-
 // register
 router.post("/register", async (req, res) => {
   try {
@@ -34,6 +28,30 @@ router.post("/register", async (req, res) => {
   
     newUser.save();
     const token = jwtGenerator(newUser._id, )
+    res.json({token})
+  } catch (error) {
+    console.error(error.message)
+    res.status(500).send("server error")
+  }
+})
+
+// login
+router.post("/login", async (req, res) => {
+  try {
+    const {email, password} = req.body
+
+    const user = await User.exists({ email: email });
+    if(user === false) {
+      return res.status(401).json("Invalid email or password")
+    }
+    let curUser = await User.find(
+      {'email': { $in: email}}
+    );
+    const validPassword = await bcrypt.compare(password, curUser[0].password)
+    if(!validPassword) {
+      return res.status(401).json("Invalid email or password")
+    }
+    const token = jwtGenerator(curUser[0]._id)
     res.json({token})
   } catch (error) {
     console.error(error.message)
