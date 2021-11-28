bffc0b7c7367c356da30b891bd8bbfa149b5b18a
Jacob Spade <spade.jacob@Yahoo.com>
2021-11-28 14:29:25 -0800

    pagination added

--- client/src/components/Paginate.jsx
+++ client/src/components/Paginate.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import {Pagination} from 'react-bootstrap'
 import {Link} from 'react-router-dom'
+import '../css/paginate.css'
 
 const Paginate = ({pages, page, keyword = ''}) => {
   return pages > 1 && (
-    <Pagination>
+    <Pagination
+      className="pagination-bar flex-wrap"
+    >
       {[...Array(pages).keys()].map(x => (
-        <a href="/jacob">hello</a>
-        // <Link
-        //   key={x + 1} 
-        //   to={keyword ? `/search/${keyword}/page/${x+1}` : `/page/${x+1}`}
-        // >
-        //   <Pagination.Item active={x+1 === page}>{x+1}</Pagination.Item>
-        // </Link>
+        <Link
+          className="pagination-link"
+          key={x + 1} 
+          to={keyword ? `/search/${keyword}/page/${x+1}` : `/page/${x+1}`}
+        >
+          <div className="pagination-box" active={x+1 === page}>{x+1}</div>
+        </Link>
       ))}
     </Pagination>
   )
